3c862a51898784d3e3b69b49bb6a507523076a90
Pedro Pachuca <pedritopachuca@me.com>
2017-05-04 14:59:26 -0700

    Fixed problems

--- src/HorizontalDivider.java
+++ src/HorizontalDivider.java
@@ -1,4 +1,29 @@
 
-public class HorizontalDivider {
+public class HorizontalDivider{
+
+	/*@Override
+	public void grow() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getLength() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public boolean collided(Collidable c) {
+		// TODO Auto-generated method stub
+		return false;
+	}*/
 
 }
--- src/VerticalDivider.java
+++ src/VerticalDivider.java
@@ -1,4 +1,28 @@
 
-public class VerticalDivider {
+public class VerticalDivider{
+
+	/*@Override
+	public boolean collided(Collidable c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void grow() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getLength() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getType() {
+		// TODO Auto-generated method stub
+		return null;
+	}*/
 
 }
