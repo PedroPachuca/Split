088c6dc6239e9e1e65a92c2bef5aba80622e0dae
Andrew Smith <andrewsmith@rocketmail.com>
2017-05-04 14:45:35 -0700

    added wall class

--- src/Wall.java
+++ src/Wall.java
@@ -1,4 +1,32 @@
 
 public class Wall {
 
+	private int x1,x2,y1,y2;
+	
+	private Wall(int x1,int x2,int y1,int y2){
+		this.x1=x1;
+		this.x2=x2;
+		this.y1=y1;
+		this.y2=y2;
+	}
+	
+	public int getX1(){
+		return x1;
+		
+	}
+	public int getX2(){
+		return x2;
+		
+	}
+	public int getY1(){
+		return y1;
+	
+	}
+	public int getY2(){
+		return y2;
+	
+	}
+	
+	
+
 }
