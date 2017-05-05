e0ddaa2f2efe45b9278be2e5374aeb6feea3733b
Pedro Pachuca <pedritopachuca@me.com>
2017-05-05 14:57:40 -0700

    Worked on SplitPanel with ball, colors, and divider

--- src/SplitPanel.java
+++ src/SplitPanel.java
@@ -1,4 +1,54 @@
+import java.awt.Color;
+import java.util.ArrayList;
 
-public class SplitPanel {
+import javax.swing.JPanel;
 
+
+public class SplitPanel extends JPanel{
+
+		// what private data is needed?
+		final int width, length;
+		private Color backgroundColor;
+		private Ball ball;
+		//private Polygon map;
+		private ArrayList<Divider> dividers = new ArrayList<Divider>();
+
+		public SplitPanel(int width, int length) {
+			this.width = width;
+			this.length = length;
+			
+			//TODO Parth make background colors
+			//backgroundColor = new Color();
+			
+			
+			beginGame();
+		}
+
+		private void beginGame() {
+			
+			createBall();
+			createMap();
+			initDividers();
+			
+		}
+
+		private void initDividers() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		private void createMap() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		private void createBall() {
+			final int ballRadius = 2;
+			ball = new Ball(0, 0, ballRadius);
+			
+		}
+		
+	
+
+	
 }
