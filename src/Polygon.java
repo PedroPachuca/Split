106d21f3f074018f9d45c0a4ff88617ceb6cf900
Andrew Smith <andrewsmith@rocketmail.com>
2017-05-09 14:55:55 -0700

    within bound clicking thing method blah blah blah

--- src/Polygon.java
+++ src/Polygon.java
@@ -17,17 +17,41 @@ public class Polygon {
         return walls;
     }
     
-   /* public boolean inside(int x, int y){
+    public boolean inside(int x, int y){
         boolean up=false;
         boolean down=false;
         boolean right=false;
         boolean left=false;
         for(Wall wall:walls){
             if(x>wall.getX()&&x<wall.getX()+wall.getWidth()){
+            	if(!(y>wall.getY()&&y<wall.getY()+wall.getLength())){
+            		if(y<wall.getY()){
+            			up=true;
+            		}
+            		else{
+            			down=true;
+            		}
+            	}
+            }
+            else if(y>wall.getY()&&y<wall.getY()+wall.getLength()){
+            	if(!(x>wall.getX()&&x<wall.getX()+wall.getWidth())){
+            		if(x<wall.getX()){
+            			right=true;
+            		}
+            		else{
+            			left=true;
+            		}
+            	}
             }
         }
+        if(up&&down&&right&&left){
+        	return true;
+        }
+        else{
+        	return false;
+        }
         
-    }*/
+    }
 
 
 }
