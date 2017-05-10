7e531b11cab595d47ccb6c2c47428eee03639a70
michael-glush <michael.glushchenko@gmail.com>
2017-05-10 14:43:41 -0700

    Progress Bar 2.0

--- src/SplitPanel.java
+++ src/SplitPanel.java
@@ -1,6 +1,8 @@
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
@@ -9,6 +11,8 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.SwingUtilities;
 import javax.swing.Timer;
 
 public class SplitPanel extends JPanel{
@@ -18,12 +22,22 @@ public class SplitPanel extends JPanel{
 		SplitGameMap gm;
 		private Timer t;
 		BufferedImage img;
+		private JProgressBar progressBar;
+		private int level;
+		private int areaAvailable;
+		private int areaCutOff;
+		private GridBagLayout layout = new GridBagLayout();
+		GridBagConstraints cons = new GridBagConstraints();
 		//private Polygon map;
 
 		public SplitPanel(int width, int length) {
 			dimensions = new Vector(width, length);
 			openBackgroundImg();
-			
+			progressBar = new JProgressBar();
+			level = 1;
+			areaAvailable = width * length;
+			setUpProgressBar(width, length);
+			areaCutOff = 5;
 			//TODO Parth make background colors
 			Color backgroundColor = Color.GREEN;
 			this.setBackground(backgroundColor);
@@ -31,7 +45,20 @@ public class SplitPanel extends JPanel{
 			
 			beginGame();
 		}
-
+		
+		private void setUpProgressBar(int width, int length) {
+			progressBar.setIndeterminate(true);
+			progressBar.setForeground(Color.orange);
+			progressBar.setBackground(new Color(0, 171, 255));
+			progressBar.setValue(5);
+			progressBar.setOpaque(true);
+			this.add(progressBar);
+			progressBar.setVisible(true);
+		}
+		
+		private void updateTimer(Graphics g) {
+			progressBar.setValue(areaCutOff);
+		}
 		private void openBackgroundImg() {
 			try {
 				img = ImageIO.read(new File("background.jpg"));
@@ -72,6 +99,7 @@ public class SplitPanel extends JPanel{
 		protected void paintComponent(Graphics g) {
 			super.paintComponent(g);
 			
+			updateTimer(g);
 			g.drawImage(img, 0, 0, dimensions.getX(), dimensions.getY(), null);
 			gm.draw(g);
 		}
