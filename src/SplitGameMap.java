14986eca4be53d396f5ec09de28ebe30ae294a8d
michael-glush <michael.glushchenko@gmail.com>
2017-05-22 14:51:49 -0700

    progress bar updates, level changes, still need to display the level tho

--- src/SplitGameMap.java
+++ src/SplitGameMap.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 
-public class SplitGameMap extends GameMap{
+public class SplitGameMap extends GameMap {
 
 	private Ball ball;
 	private Vector dimensions;
@@ -16,10 +16,15 @@ public class SplitGameMap extends GameMap{
 	private int cushion = 10;
 	private boolean ready = true;
 	private Image gameOver;
+	private final int STARTWIDTH;
+	private final int STARTHEIGHT;
+
 	public SplitGameMap(Vector dims) {
 		dimensions = dims;
 		createBall();
 		createWalls();
+		STARTHEIGHT = polygon.getHeight();
+		STARTWIDTH = polygon.getWidth();
 		File file = new File("src/THEBACKGROUND.jpg");
 		try {
 			gameOver = ImageIO.read(file);
@@ -28,37 +33,53 @@ public class SplitGameMap extends GameMap{
 			e.printStackTrace();
 		}
 	}
-	private void createWalls() {
-
-		polygon.add(new Wall(cushion*2,cushion*2,cushion,dimensions.getY()-150-2*cushion));
-        polygon.add(new Wall(cushion*2,dimensions.getY()-150-cushion,dimensions.getX()-4*cushion,cushion));
-        polygon.add(new Wall(cushion*2,cushion*2,dimensions.getX()-4*cushion,cushion));
-        polygon.add(new Wall(dimensions.getX()-cushion*3,cushion*2,cushion,dimensions.getY()-150-2*cushion));
-        //polygon = polygon.split(cushion*2+3,300,dimensions.getX()-cushion*3+3,300,700,100,new Wall(cushion*2, 300,dimensions.getX()-cushion*3+3-cushion*2,cushion),null);
-		//updateAllPolygons();
+	
+	public int getStartWidth() {
+		return STARTWIDTH;
 	}
 	
+	public int getStartHeight() {
+		return STARTHEIGHT;
+	}
+	
+	private void createWalls() {
+
+		polygon.add(new Wall(cushion * 2, cushion * 2, cushion, dimensions.getY() - 150 - 2 * cushion));
+		polygon.add(new Wall(cushion * 2, dimensions.getY() - 150 - cushion, dimensions.getX() - 4 * cushion, cushion));
+		polygon.add(new Wall(cushion * 2, cushion * 2, dimensions.getX() - 4 * cushion, cushion));
+		polygon.add(
+				new Wall(dimensions.getX() - cushion * 3, cushion * 2, cushion, dimensions.getY() - 150 - 2 * cushion));
+		
+		// polygon =
+		// polygon.split(cushion*2+3,300,dimensions.getX()-cushion*3+3,300,700,100,new
+		// Wall(cushion*2,
+		// 300,dimensions.getX()-cushion*3+3-cushion*2,cushion),null);
+		// updateAllPolygons();
+	}
+
 	private void updateAllPolygons() {
 		ball.updatePolygon(polygon);
-		for(Divider div: dividers) {
+		for (Divider div : dividers) {
 			div.updatePolygon(polygon);
 		}
 	}
-	
+
 	public Ball getBall() {
 		return ball;
 	}
+
 	public void ready() {
 		ready = true;
 	}
+
 	@Override
 	public void tick() {
 		// TODO Auto-generated method stub
 		ball.move();
-		for(Wall w: polygon.walls()) {
+		for (Wall w : polygon.walls()) {
 			w.collided(ball);
 		}
-		for(Divider div: dividers) {
+		for (Divider div : dividers) {
 			div.grow();
 			div.collided(ball);
 		}
@@ -68,35 +89,35 @@ public class SplitGameMap extends GameMap{
 	public void draw(Graphics g) {
 		if (ball.getAlive()) {
 			ball.draw(g);
-			for(Divider div: dividers) {
+			for (Divider div : dividers) {
 				div.draw(g);
 			}
-			for(Wall w: polygon.walls()) {
+			for (Wall w : polygon.walls()) {
 				w.draw(g);
 			}
-		}
-		else {
+		} else {
 			g.drawImage(gameOver, 0, 0, null);
 		}
 	}
-	
+
 	private void createBall() {
-		
+
 		final int ballRadius = 100;
 		ball = new Ball(dimensions.getX() / 3, dimensions.getY() / 3, ballRadius, polygon);
-		
+
 	}
-	
+
 	public void addDivider(Divider div) {
-		if(ready) {
-		dividers.clear();
-		dividers.add(div);
+		if (ready) {
+			dividers.clear();
+			dividers.add(div);
 		}
 	}
+
 	public Polygon getPolygon() {
 		return polygon;
 	}
-	
+
 	public void newSplit(Polygon newPolygon) {
 		polygon = newPolygon;
 		updateAllPolygons();
--- src/SplitPanel.java
+++ src/SplitPanel.java
@@ -25,249 +25,256 @@ import javax.swing.Timer;
 
 public class SplitPanel extends JPanel{
 
-		// what private data is needed?
-		final Vector dimensions;
-		SplitGameMap gm;
-		private Timer t;
-		BufferedImage img;
-		private BufferedImage vertical, horizontal;
-		private int level;
-		private int areaAvailable;
-		private JProgressBar progressBar;
-		private int areaCutOff;
-		private String typeOfDivider = null;
-		//private Polygon map;
+	// what private data is needed?
+	final Vector dimensions;
+	SplitGameMap gm;
+	private Timer t;
+	BufferedImage img;
+	private BufferedImage vertical, horizontal;
+	private int level;
+	private int areaAvailable;
+	private JProgressBar progressBar;
+	private int areaCutOff;
+	private String typeOfDivider = null;
+	//private Polygon map;
 
-		public SplitPanel(int width, int length) {
-			this.setLayout(new BorderLayout());
-			dimensions = new Vector(width, length);
-			openBackgroundImg();
-			progressBar = new JProgressBar();
-			level = 1;
-			areaAvailable = width * length;
-			setUpProgressBar(width, length);
-			areaCutOff = 5;
-			//TODO Parth make background colors
-			Color backgroundColor = Color.GREEN;
-			this.setBackground(backgroundColor);
-			
-			
-			this.addMouseListener(new MouseListener() {
+	public SplitPanel(int width, int length) {
+		this.setLayout(new BorderLayout());
+		dimensions = new Vector(width, length);
+		openBackgroundImg();
+		//TODO Parth make background colors
+		Color backgroundColor = Color.GREEN;
+		this.setBackground(backgroundColor);
 
-				@Override
-				public void mouseClicked(MouseEvent arg0) {
-					// TODO Auto-generated method stub
-					
-				}
 
-				@Override
-				public void mouseEntered(MouseEvent arg0) {
-					// TODO Auto-generated method stub
-					
-				}
+		this.addMouseListener(new MouseListener() {
 
-				@Override
-				public void mouseExited(MouseEvent arg0) {
-					// TODO Auto-generated method stub
-					
-				}
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				// TODO Auto-generated method stub
 
-				@Override
-				public void mousePressed(MouseEvent click) {
-					Divider div = null;
-					if(gm.getPolygon().inside(click.getX(), click.getY())) {
-						if(typeOfDivider != null) {
-							if(typeOfDivider.equals("vertical")) {
-								div = new VerticalDivider(click.getX(), click.getY(), gm.getPolygon(), gm.getBall(), gm);
-							}
-							else {
-								div = new HorizontalDivider(click.getX(), click.getY(), gm.getPolygon(), gm.getBall(), gm);
-							}
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseExited(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mousePressed(MouseEvent click) {
+				Divider div = null;
+				if(gm.getPolygon().inside(click.getX(), click.getY())) {
+					if(typeOfDivider != null) {
+						if(typeOfDivider.equals("vertical")) {
+							div = new VerticalDivider(click.getX(), click.getY(), gm.getPolygon(), gm.getBall(), gm);
+						}
+						else {
+							div = new HorizontalDivider(click.getX(), click.getY(), gm.getPolygon(), gm.getBall(), gm);
 						}
 					}
-					
-					if(div != null) {
-						gm.addDivider(div);
-					}
 				}
 
-				@Override
-				public void mouseReleased(MouseEvent arg0) {
-					// TODO Auto-generated method stub
-					
+				if(div != null) {
+					gm.addDivider(div);
 				}
-			});
-		
-			beginGame();			
-		}
-
-		private void openBackgroundImg() {
-			try {
-				img = ImageIO.read(new File("src/background.jpg"));
 			}
-			catch(IOException e) {
-				System.out.println("Unable to instantiate background");
+
+			@Override
+			public void mouseReleased(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+
 			}
-			
+		});
+
+		beginGame();
+		areaAvailable = gm.getPolygon().getHeight() * gm.getPolygon().getWidth();
+		progressBar = new JProgressBar();
+		setUpProgressBar(width, length);
+		areaCutOff = 0;
+	}
+
+	private void openBackgroundImg() {
+		try {
+			img = ImageIO.read(new File("src/background.jpg"));
 		}
-		
-		private void setUpProgressBar(int width, int length) {			
-			JPanel barPanel = new JPanel();
-			barPanel.setLayout(new GridLayout());
-			barPanel.setPreferredSize(new Dimension(100, 20));
-			progressBar.setMaximum(areaAvailable / 2);
-			progressBar.setMinimum(0);
-			progressBar.setOpaque(true);
-			progressBar.setStringPainted(true);
-			progressBar.setBackground(Color.blue);
-			progressBar.setForeground(Color.GRAY);
-			progressBar.setVisible(true);
-			barPanel.add(progressBar);
-			this.add(barPanel,BorderLayout.NORTH);
-		}
-		
-		private void updateBar(Graphics g) {
-			progressBar.setValue(areaCutOff);
+		catch(IOException e) {
+			System.out.println("Unable to instantiate background");
 		}
 
-		private void beginGame() {
-			this.setPreferredSize(new Dimension(dimensions.getX(), dimensions.getY()));
-			createMap();
-			createDividerButtons();
-			startTicks();
-			
-		}
+	}
 
-		private void createDividerButtons() {
-			openImages();
-			
-			JPanel buttonPanel = new JPanel();
-			buttonPanel.setLayout(new FlowLayout());
-			
-			JButton horizontalDividerButton = new JButton();
-			JButton verticalDividerButton = new JButton();
-			
-			horizontalDividerButton.setIcon(new ImageIcon(horizontal));
-			verticalDividerButton.setIcon(new ImageIcon(vertical));
-			
-			horizontalDividerButton.addMouseListener(new MouseListener() {
+	private void setUpProgressBar(int width, int length) {			
+		JPanel barPanel = new JPanel();
+		barPanel.setLayout(new GridLayout());
+		barPanel.setPreferredSize(new Dimension(100, 20));
+		progressBar.setMaximum(areaAvailable / 2);
+		progressBar.setMinimum(0);
+		progressBar.setOpaque(true);
+		progressBar.setStringPainted(true);
+		progressBar.setBackground(Color.blue);
+		progressBar.setForeground(Color.GRAY);
+		progressBar.setVisible(true);
+		barPanel.add(progressBar);
+		this.add(barPanel,BorderLayout.NORTH);
+	}
 
-				@Override
-				public void mouseClicked(MouseEvent arg0) {
-					// TODO Auto-generated method stub
-					
-				}
-
-				@Override
-				public void mouseEntered(MouseEvent arg0) {
-					// TODO Auto-generated method stub
-					
-				}
-
-				@Override
-				public void mouseExited(MouseEvent arg0) {
-					// TODO Auto-generated method stub
-					
-				}
-
-				@Override
-				public void mousePressed(MouseEvent arg0) {
-					typeOfDivider = "horizontal";
-					
-				}
-
-				@Override
-				public void mouseReleased(MouseEvent arg0) {
-					// TODO Auto-generated method stub
-					
-				}
-			});
-			
-			verticalDividerButton.addMouseListener(new MouseListener() {
-
-				@Override
-				public void mouseClicked(MouseEvent e) {
-					// TODO Auto-generated method stub
-					
-				}
-
-				@Override
-				public void mouseEntered(MouseEvent e) {
-					// TODO Auto-generated method stub
-					
-				}
-
-				@Override
-				public void mouseExited(MouseEvent e) {
-					// TODO Auto-generated method stub
-					
-				}
-
-				@Override
-				public void mousePressed(MouseEvent e) {
-					typeOfDivider = "vertical";
-					
-				}
-
-				@Override
-				public void mouseReleased(MouseEvent e) {
-					// TODO Auto-generated method stub
-					
-				}
-			});
-			
-			buttonPanel.add(horizontalDividerButton);
-			buttonPanel.add(verticalDividerButton);
-			
-			buttonPanel.setBackground(Color.CYAN);
-			//TODO PICK COLOR
-			
-			this.add(buttonPanel,BorderLayout.SOUTH);
-		}
-
-		private void openImages() {
-			try {
-				horizontal = ImageIO.read(new File("src/hoirzontal.png"));
-			}
-			catch(IOException e) {
-				System.out.println("Unable to instantiate horiozntal");
-			}
-			try {
-				vertical = ImageIO.read(new File("src/vertical.png"));
-			}
-			catch(IOException e) {
-				System.out.println("Unable to instantiate vertical");
-			}
-			
-		}
-
-		private void startTicks() {
-			t = new Timer(50, new ActionListener() {// fires off every 10 ms
-				@Override
-				public void actionPerformed(ActionEvent arg0) {
-					gm.tick();// I tell the GameMap to tick... do what
-						// you do every time the clock goes off.
-					repaint();// naturally, we want to see the new view
-					Toolkit.getDefaultToolkit().sync();
-					//LESS LAG WOOHOOO
-					//This sketchy stuff requests a mouse location every frame so it repaints
-			        //MouseInfo.getPointerInfo().getLocation();
-				}
-			});
-			t.start();
-		}
-
-		private void createMap() {
+	public void updateBar() {
+		//			System.out.println(areaCutOff);
+		//			System.out.println(progressBar.getMaximum());
+		if (areaCutOff * 2 >= areaAvailable) {
 			gm = new SplitGameMap(dimensions);
+			areaCutOff = 0;
+			level++;
 		}
-		
-		@Override
-		protected void paintComponent(Graphics g) {
-			super.paintComponent(g);
-			
-			updateBar(g);
-			g.drawImage(img, 0, 0, dimensions.getX(), dimensions.getY(), null);
-			gm.draw(g);
+		areaCutOff = areaAvailable - gm.getPolygon().getHeight() * gm.getPolygon().getWidth();
+		progressBar.setValue(areaCutOff);
+	}
+
+	private void beginGame() {
+		this.setPreferredSize(new Dimension(dimensions.getX(), dimensions.getY()));
+		createMap();
+		createDividerButtons();
+		startTicks();
+
+	}
+
+	private void createDividerButtons() {
+		openImages();
+
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new FlowLayout());
+
+		JButton horizontalDividerButton = new JButton();
+		JButton verticalDividerButton = new JButton();
+
+		horizontalDividerButton.setIcon(new ImageIcon(horizontal));
+		verticalDividerButton.setIcon(new ImageIcon(vertical));
+
+		horizontalDividerButton.addMouseListener(new MouseListener() {
+
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseExited(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+				typeOfDivider = "horizontal";
+
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
+		verticalDividerButton.addMouseListener(new MouseListener() {
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				typeOfDivider = "vertical";
+
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
+		buttonPanel.add(horizontalDividerButton);
+		buttonPanel.add(verticalDividerButton);
+
+		buttonPanel.setBackground(Color.CYAN);
+		//TODO PICK COLOR
+
+		this.add(buttonPanel,BorderLayout.SOUTH);
+	}
+
+	private void openImages() {
+		try {
+			horizontal = ImageIO.read(new File("src/hoirzontal.png"));
 		}
-	
+		catch(IOException e) {
+			System.out.println("Unable to instantiate horiozntal");
+		}
+		try {
+			vertical = ImageIO.read(new File("src/vertical.png"));
+		}
+		catch(IOException e) {
+			System.out.println("Unable to instantiate vertical");
+		}
+
+	}
+
+	private void startTicks() {
+		t = new Timer(50, new ActionListener() {// fires off every 10 ms
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				gm.tick();// I tell the GameMap to tick... do what
+				// you do every time the clock goes off.
+				repaint();// naturally, we want to see the new view
+				Toolkit.getDefaultToolkit().sync();
+				//LESS LAG WOOHOOO
+				//This sketchy stuff requests a mouse location every frame so it repaints
+				//MouseInfo.getPointerInfo().getLocation();
+			}
+		});
+		t.start();
+	}
+
+	private void createMap() {
+		gm = new SplitGameMap(dimensions);
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+		updateBar();
+		g.drawImage(img, 0, 0, dimensions.getX(), dimensions.getY(), null);
+		gm.draw(g);
+	}
+
 }
